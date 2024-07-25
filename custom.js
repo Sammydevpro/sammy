e35f3d92210a4be30f941fcd4df4005a808d096e
Sammydevpro <devsammy3@gmail.com>
2024-07-25 06:00:03 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -31,3 +31,28 @@
   })(window.jQuery);
 
 
+function emailSend(){
+
+  var userName = document.getElementById('name').value;
+  var phone = document.getElementById('phone').value;
+  var email = document.getElementById('email').value;
+
+  var messageBody = "Name " + userName +
+  "<br/> Phone " + phone +
+  "<br/> Email " + email;
+  
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "devsammy3@gmail.com",
+    Password : "9368BF28B8BFAEF57B4C00B6B8031C552F1F",
+    To : 'devsammy3@gmail.com',
+    From : "devsammy3@gmail.com",
+    Subject : "This is the subject",
+    Body : "messageBody"
+}).then(
+  message => alert(message)
+);
+
+}
+
+
